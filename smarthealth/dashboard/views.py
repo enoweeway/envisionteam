d28363330c66ecd4f1d6d3ffe0ea8b345045a6f2
Noey <ignacionooey@gmail.com>
2020-02-26 01:33:22 +0800

    Patient Count

--- smarthealth/dashboard/views.py
+++ smarthealth/dashboard/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from patients.models import Patient
 
 def patientsDashboard(request):
 
-    return render(request, 'dashboards/views/patient_dashboard.html', {})
+    patient = Patient.objects.all()
+
+    total_patients = patient.count()
+
+    context = {
+        'total_patients' : total_patients
+    }
+
+    return render(request, 'dashboards/views/patient_dashboard.html', context)
+
 
 def analyticsDasboard(request):
 
